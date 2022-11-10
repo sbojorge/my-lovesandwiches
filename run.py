57957f24d79eff8f314022dedb8056fb9846472e
sbojorge <sara.bojorge@gmail.com>
2022-11-10 18:25:32 +0000

    Add users request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -10,18 +7,30 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+
 def get_sales_data():
-    """    
+    """
     Get sales data input from the user
     """
-    print('Please enter sales data from the last market')
-    print('Data should be 6 numbers separated by commas')
-    print('Example: 10, 20, 30, 40, 50, 60\n')
+    while True:
+        print('Please enter sales data from the last market')
+        print('Data should be 6 numbers separated by commas')
+        print('Example: 10, 20, 30, 40, 50, 60\n')
+    
+        data_str = input('Enter your data here:')
+    
+        sales_data = data_str.split(",")
+            
+        if validate_data(sales_data):
+            print('Data is valid!')
+            break
 
-    data_str = input('Enter your data here:')
-
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+    return sales_data
 
 
 def validate_data(values):
@@ -29,7 +38,7 @@ def validate_data(values):
     Inside the try, converts all string values into integers.
     Raises ValueError if string can not be converted
     or if there are not exactly 6 values
-    """ 
+    """
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -38,5 +47,9 @@ def validate_data(values):
                 )
     except ValueError as e:
         print(f'Invalid data: {e}, please try again\n')
+        return False
 
-get_sales_data()
+    return True
+
+
+data = get_sales_data()
